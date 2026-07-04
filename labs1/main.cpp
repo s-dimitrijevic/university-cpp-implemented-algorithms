f198f47ff4d9b87cc479272fe3c74e3258af579d
= <=>
2026-07-04 17:01:56 +0200

    labs1

--- labs1/main.cpp
+++ labs1/main.cpp
@@ -38,5 +38,27 @@ int main()
     std::cout << "sizeof(vrednost1): " << sizeof(vrednost1) << std::endl;
     std::cout << "sizeof(vrednost2): " << sizeof(vrednost2) << std::endl;
 
+    float nekiTamoFloat{ 1.123234587423f};
+    double nekiTamoDouble{ 1.123234587123423 };
+    long double nekiTamoLongDouble{ 1.1232345874234567L };
+
+    std::cout << "sizeof(nekiTamoFloat): " << sizeof(nekiTamoFloat) << std::endl;
+    std::cout << "sizeof(nekiTamoDouble): " << sizeof(nekiTamoDouble) << std::endl;
+    std::cout << "sizeof(nekiTamoLongDouble): " << sizeof(nekiTamoLongDouble) << std::endl;
+
+    bool crveno_svetlo{ false };
+    bool zeleno_svetlo{ true };
+
+    if (crveno_svetlo == true){
+        std::cout << "STOP!!!" << std::endl;
+    }
+    else{
+        std::cout << "Mozes da prodjes" << std::endl;
+    }
+
+    std::cout << "sizeof(bool): " << sizeof(bool) << std::endl;
+    std::cout << "Crveno svetlo: " << crveno_svetlo << std::endl;
+    std::cout << "Zeleno svetlo: " << zeleno_svetlo << std::endl;
+
     return 0;
 }
